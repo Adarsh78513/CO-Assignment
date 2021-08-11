9384af910d5c7706883f10567bcee98d81508e4f
Akshat <akshat20172@iiitd.ac.in>
2021-08-11 11:56:49 +0530

    Update simulator.java

--- CO_M21_Assignment-main/SimpleSimulator/simulator.java
+++ CO_M21_Assignment-main/SimpleSimulator/simulator.java
@@ -1,8 +1,80 @@
 import java.util.Scanner;
 
 public class simulator {
+
+    public static String add() {
+
+    }
+
+    public static String sub() {
+
+    }
+
+    public static String movImm() {
+
+    }
+
+    public static String movReg() {
+
+    }
+
+    public static void load() {
+
+    }
+
+    public static void store() {
+
+    }
+
+    public static String mul() {
+
+    }
+
+    public static String div() {
+
+    }
+
+    public static String bitwiseXOR() {
+
+    }
+
+    public static String bitwiseOR() {
+
+    }
+
+    public static String bitwiseAND() {
+
+    }
+
+    public static String bitwiseNOT() {
+
+    }
+
+    public static boolean compare() {
+
+    }
+
+    public static void jump() {
+        
+    }
+    
+    public static void jumpLessThan() {
+        
+    }
+
+    public static void jumpGreaterThan() {
+        
+    }
+
+    public static void jumpEqual() {
+        
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        while (!in.nextLine().equals(null)) {
+            
+        }
+        in.close();
     }
 }
