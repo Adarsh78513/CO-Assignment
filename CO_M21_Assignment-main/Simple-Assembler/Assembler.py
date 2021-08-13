808fbe32df52b4cc3f7a2f518b0bc7b68910d955
Akshat <akshat20172@iiitd.ac.in>
2021-08-13 11:40:35 +0530

    Update Assembler.py

--- CO_M21_Assignment-main/Simple-Assembler/Assembler.py
+++ CO_M21_Assignment-main/Simple-Assembler/Assembler.py
@@ -1,16 +1,36 @@
-opcode = {"add": "00000", "sub": "00001", "movi": "00010", "movr": "00011", "ld": "00100",
-          "st": "00101", "mul": "00110", "div": "00111", "rs": "01000", " ls": "01001",
-          "xor": "01010", "or": "01011", "and": "01100", "not": "01101", "cmp": "01110",
-          "jmp": "01111", "jlt": "10000", "jgt": "10001", "je": "10010", "hlt": "10011"}
+opcode = {
+    "add": ("00000", "A"),
+    "sub": ("00001", "A"),
+    "movi": ("00010", "B"),
+    "movr": ("00011", "C"),
+    "ld": ("00100", "D"),
+    "st": ("00101", "D"),
+    "mul": ("00110", "A"),
+    "div": ("00111", "C"),
+    "rs": ("01000", "B"),
+    "ls": ("01001", "B"),
+    "xor": ("01010", "A"),
+    "or": ("01011", "A"),
+    "and": ("01100", "A"),
+    "not": ("01101", "C"),
+    "cmp": ("01110", "C"),
+    "jmp": ("01111", "E"),
+    "jlt": ("10000", "E"),
+    "jgt": ("10001", "E"),
+    "je": ("10010", "E"),
+    "hlt": ("10011", "F")
+}
 
-register = {"R0": "000", "R1": "001", "R2": "010", "R3": "011", "R4": "100", "R5": "101",
-            "R6": " 110", "FLAGS": "111"}
-
-types = {"add": "A", "sub": "A", "movi": "B", "movr": "C", "ld": "D",
-         "st": "D", "mul": "A", "div": "C", "rs": "B", " ls": "B",
-         "xor": "A", "or": "A", "and": "A", "not": "C", "cmp": "C",
-         "jmp": "E", "jlt": "E", "jgt": "E", "je": "E", "hlt": "F"}
-
+register = {
+    "R0": "000", 
+    "R1": "001", 
+    "R2": "010", 
+    "R3": "011", 
+    "R4": "100", 
+    "R5": "101",
+    "R6": " 110", 
+    "FLAGS": "111"
+}
 
 def decToBinary(n):
     s = bin(n)
@@ -28,18 +48,18 @@ def assemblyCode(line):
         else:
             inst[0] = "movi"
     #
-    if types[inst[0]] == "A":
-        return(opcode[inst[0]] + "0" * 2 + register[inst[1]] + register[inst[2]] + register[inst[3]])
-    elif types[inst[0]] == "B":
-        return(opcode[inst[0]] + register[inst[1]] + decToBinary(int(inst[2][1::])))
-    elif types[inst[0]] == "C":
-        return(opcode[inst[0]] + "0" * 5 + register[inst[1]] + register[inst[2]])
-    elif types[inst[0]] == "D":
-        return(opcode[inst[0]] + "0" * 3 + decToBinary(int(inst[1])))
-    elif types[inst[0]] == "E":
-        return(opcode[inst[0]] + "0" * 3 + decToBinary(int(inst[1])))
-    elif types[inst[0]] == "F":
-        return(opcode[inst[0]] + "0" * 11)
+    if opcode[inst[0][1]] == "A":
+        return(opcode[inst[0][0]] + "0" * 2 + register[inst[1]] + register[inst[2]] + register[inst[3]])
+    elif opcode[inst[0][1]] == "B":
+        return(opcode[inst[0][0]] + register[inst[1]] + decToBinary(int(inst[2][1::])))
+    elif opcode[inst[0][1]] == "C":
+        return(opcode[inst[0][0]] + "0" * 5 + register[inst[1]] + register[inst[2]])
+    elif opcode[inst[0][1]] == "D":
+        return(opcode[inst[0][0]] + "0" * 3 + decToBinary(int(inst[1])))
+    elif opcode[inst[0][1]] == "E":
+        return(opcode[inst[0][0]] + "0" * 3 + decToBinary(int(inst[1])))
+    elif opcode[inst[0][1]] == "F":
+        return(opcode[inst[0][0]] + "0" * 11)
 
 
 # st = input("code:  ")
