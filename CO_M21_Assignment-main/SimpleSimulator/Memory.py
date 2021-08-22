5ecbda285a9470b3afb2de08a75fadac4521e3a5
Akshat <akshat20172@iiitd.ac.in>
2021-08-23 01:24:26 +0530

    PC fixed a bit

--- CO_M21_Assignment-main/SimpleSimulator/Memory.py
+++ CO_M21_Assignment-main/SimpleSimulator/Memory.py
@@ -5,10 +5,20 @@ while True:
         temp = input()
         if len(temp) == 0:
             break
+        instList.append(temp)
     except EOFError:
         break
-    instList.append(temp)
+    
+def binToDec(num):
+    """[summary]
 
+    Args:
+        num ([string]): [binary number]
+
+    Returns:
+        [integer]: [description]
+    """
+    return int(num, 2)
 
 class Memory:
     def __init__(self):
@@ -24,7 +34,7 @@ class Memory:
             mem[index] = self.instList[index]
 
     def get(self, index, cycle):
-        return self.mem[int(index)]
+        return self.mem[binToDec(str(index))]
 
     def dump(self):
         for k in self.mem:
